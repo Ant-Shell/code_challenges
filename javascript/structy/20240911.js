00cec8be6cf46bfe60e4eb9f690125efa9b98b9a
Anthony Shellman <atshellman@gmail.com>
2024-09-11 13:49:06 -0600

    Add pseudocode

--- javascript/structy/20240911.js
+++ javascript/structy/20240911.js
@@ -25,3 +25,12 @@ const pairSum = (numbers, targetSum) => {
 console.log(pairSum([3, 2, 5, 4, 1], 8)) // -> [0, 2]
 console.log(pairSum([9, 9], 18)) // -> [0, 1]
 console.log(pairSum([6, 4, 2, 8 ], 12)) // -> [1, 3]
+
+/*
+Pseudocode:
+- Initialize an hashmap object named previous
+- Loop through the numbers array and for each value, subtract the targetSum by the current value in the loop; assign to complement variable
+- If compliment appears as a key in previous
+  - Return an array that is the value of: [previous with key compliment, index of the current iteration in the numbers loop]
+- Add a key/value pair to previous: {current value in the loop: current index in the loop}
+*/
