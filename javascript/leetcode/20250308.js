32c6aef82ed05bc1514a517ac855bf929e999098
Anthony Shellman <atshellman@gmail.com>
2025-03-08 17:48:55 -0700

    Add javascript challenge for the day 20250308

---
+++ javascript/leetcode/20250308.js
@@ -0,0 +1,87 @@
+/* 
+https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/description/
+
+Minimum Recolors to Get K Consecutive Black Blocks
+
+You are given a 0-indexed string blocks of length n, where blocks[i] is either 'W' or 'B', representing the color of the ith block. The characters 'W' and 'B' denote the colors white and black, respectively.
+
+You are also given an integer k, which is the desired number of consecutive black blocks.
+
+In one operation, you can recolor a white block such that it becomes a black block.
+
+Return the minimum number of operations needed such that there is at least one occurrence of k consecutive black blocks.
+
+Example 1:
+
+Input: blocks = "WBBWWBBWBW", k = 7
+Output: 3
+Explanation:
+One way to achieve 7 consecutive black blocks is to recolor the 0th, 3rd, and 4th blocks
+so that blocks = "BBBBBBBWBW". 
+It can be shown that there is no way to achieve 7 consecutive black blocks in less than 3 operations.
+Therefore, we return 3.
+
+Example 2:
+
+Input: blocks = "WBWBBBW", k = 2
+Output: 0
+Explanation:
+No changes need to be made, since 2 consecutive black blocks already exist.
+Therefore, we return 0.
+ 
+
+Constraints:
+
+n == blocks.length
+1 <= n <= 100
+blocks[i] is either 'W' or 'B'.
+1 <= k <= n
+
+*/
+
+const minimumRecolors = (blocks, k) => {
+  let start = 0
+  let end = start + k
+  let recolors = 0
+  let minRecolors = Number.POSITIVE_INFINITY
+
+  while (end <= blocks.length) {
+    for (let i = start ; i < end ; i++) {
+      if (blocks[i] === 'W') {
+        recolors += 1
+      }
+    }
+    minRecolors = Math.min(recolors, minRecolors)
+    recolors = 0
+    start += 1
+    end = start + k
+  }
+  return minRecolors
+}
+
+console.log(minimumRecolors("WBBWWBBWBW", 7))
+console.log(minimumRecolors("WBWBBBW", 2))
+
+/* 
+Pseudocode:
+- Initialize start as zero
+- Initialize end as start + k (window)
+- Initialize recolors as zero
+- Initialize minRecolors as positive infinity
+- While end is less than or equal to blocks.length:
+  - Iterate through the blocks substring:
+    - If blocks[i] === 'W'
+      - Increment recolors by one
+  - Reassign minRecolors as the minimum between recolors and minRecolors
+  - Reassign recolors to zero
+  - Increment start by one
+  - Reassign end to start + k
+- Return minRecolors
+
+- Time complexity = O(N * K)
+- Space complexity = O(n)
+
+- Room for improvement:
+  - Recounting the number of 'W' characters for each substring
+    is taxing - this code could be refactored
+*/
