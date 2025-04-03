2acc74b24e91ab4770ceba0027c4fe10554a0052
Anthony Shellman <atshellman@gmail.com>
2025-04-03 13:49:55 -0600

    Add javascript challenge for the day 20250403

---
+++ javascript/leetcode/20250403.js
@@ -0,0 +1,63 @@
+/* 
+https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/
+
+Maximum Value of an Ordered Triplet II
+
+You are given a 0-indexed integer array nums.
+
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+
+Example 1:
+
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77.
+
+Example 2:
+
+Input: nums = [1,10,3,4,19]
+Output: 133
+Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+It can be shown that there are no ordered triplets of indices with a value greater than 133.
+
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+*/
+
+const maximumTripletValue = (nums) => {
+  let maxNumber = 0
+  let maxDifference = 0
+  let maxResult = 0
+
+  for (let i = 0 ; i < nums.length ; i++) {
+    maxResult = Math.max(maxResult, maxDifference * nums[i])
+    maxDifference = Math.max(maxDifference, maxNumber - nums[i])
+    maxNumber = Math.max(maxNumber, nums[i])
+  }
+
+  return maxResult
+}
+
+console.log(maximumTripletValue([12,6,1,2,7])) // -> 77
+console.log(maximumTripletValue([1,10,3,4,19])) // -> 133
+console.log(maximumTripletValue([1,2,3])) // -> 0
+
+/* 
+Pseudocode:
+- Initialize maxNumber, maxDifference, maxResult
+- Iterate through the nums array
+  - Reassign maxResult as the max between maxResult and maxDifference * nums[i]
+  - Reassign maxDifference as the max between maxDifference ans maxNumber - num[i]
+  - Reassign maxNumber as the max between maxNumber and nums[i]
+- Return maxResult
+
+- Time complexity: O(n)
+- Space complexity: O(1)
+*/
